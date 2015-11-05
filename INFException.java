a19d6a3522cb6e6769add10661f67f4d214e2f75
jasongefre <jason.gefre@gmail.com>
2015-11-04 23:24:40 -0500

    created

---
+++ INFException.java
@@ -0,0 +1,11 @@
+package assignment4;
+
+/**
+ * Created by JMG on 11/1/2015.
+ */
+public class INFException extends Exception {
+    public INFException(String message)
+    {
+        super(message);
+    }
+}
